091bb90f9b3b5728d328774861383c5129fa6fca
duck-tech <nvviet.dn@gmail.com>
2020-05-05 09:16:20 +0700

    Font Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'tab1.dart';
-import 'tab2.dart';
-import 'tab3.dart';
-import 'tab4.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,41 +6,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    final tabController = new DefaultTabController(
-        length: 4,  //số tab
-        child: new Scaffold(
-          appBar: new AppBar(
-            title: new Text("These are tabs"),
-            bottom: new TabBar(
-              indicatorColor: Colors.amberAccent,
-              indicatorWeight: 2.0,
-              tabs: [
-                new Tab(
-                  icon: new Icon(Icons.home),
-                  text: "Home",),
-                new Tab(
-                  icon: new Icon(Icons.group),
-                  text: "Group",),
-                new Tab(
-                  icon: new Icon(Icons.chat_bubble),
-                  text: "Chat",),
-                new Tab(
-                  icon: new Icon(Icons.cloud),
-                  text: "Cloud",),
-              ],
-            ),
-          ),
-          body: new TabBarView(children: [
-            new Tab1(),
-            new Tab2(),
-            new Tab3(),
-            new Tab4(),
-          ]),
-        )
-    );
-    return new MaterialApp(
+    return new MaterialApp (
       title: "",
-      home: tabController
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Font"),
+        ),
+        body: new Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            new Text(
+                "This is Galada font",
+                style: new TextStyle(
+                  fontFamily: "Galada",
+                  fontSize: 30.0
+                ),
+              ),
+            new Text(
+              "This is JosefinSans font",
+              style: new TextStyle(
+                  fontFamily: "JosefinSans",
+                  fontSize: 30.0
+              ),
+            ),
+            new Text(
+              "This is MontserratSubrayada font",
+              style: new TextStyle(
+                  fontFamily: "MontserratSubrayada",
+                  fontSize: 30.0
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
